b5f0dba430b9bacffef538bf98b58c190300dfbe
MendySilberstrom <mzilbi@gmail.com>
2023-06-05 18:11:10 +0300

    :recycle: scanner - scan lazy - scan bytes

--- scanner/modules/scanner/scanner.py
+++ scanner/modules/scanner/scanner.py
@@ -1,12 +1,24 @@
-
 from .service import Service
-   
+
+_max_bytes__buffer = (1024 * 1024)
+
+
 async def is_blacklist_in_text(text):
-        blacklist = await Service.fetch_api_config()
-        print(f"blacklist: {blacklist}")
-        return any(black_word in text for black_word in blacklist)
-    
+    blacklist = await Service.fetch_api_config()
+    print(f"blacklist: {blacklist}")
+    for black_word in blacklist:
+        black_word_bytes = black_word.encode()
+        if black_word_bytes in text:
+            return True
+    return False
+
+
 async def is_blacklist_in_file(file):
-        content = file.read().decode("utf-8")
+    while True:
+        content = file.read(_max_bytes__buffer)
+        if not content:
+            break
         print(f"file content: {content}")
-        return await is_blacklist_in_text(content)
+        if await is_blacklist_in_text(content):
+            return True
+    return False
